48cb63fada9946a3b215911481de7bc4b073b822
Srđan Stanić <srstanic@gmail.com>
2015-02-27 20:21:02 +0100

    Playing with the port.
    Heroku assigns port dynamically through environment.

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ var requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Hello, World!\n');
 }
-
-var server = http.createServer(requestListener).listen(8080);
+var port = process.env.PORT || 8080;
+console.log(port);
+var server = http.createServer(requestListener).listen(port);
