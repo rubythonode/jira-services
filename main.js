ff16e15a3395d72d546074e2ad60a6f81f50a959
Srđan Stanić <srstanic@gmail.com>
2015-02-27 19:28:04 +0100

    Adds dummy server.

---
+++ main.js
@@ -0,0 +1,9 @@
+http = require('http');
+jira = require('jira')
+
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
+
+var server = http.createServer(requestListener).listen(8080);
